350aaaf300e7b771a4413eb190aba2f99de2ed25
Illia1044 <92012282+Illia1044@users.noreply.github.com>
2022-05-30 21:16:40 +0300

    Add files via upload

---
+++ TUIdemo/src/main/java/domain/OverDraftAmountException.java
@@ -0,0 +1,20 @@
+package domain;
+
+/**
+ *
+ * @author taurus
+ */
+public class OverDraftAmountException extends Exception {
+
+    private double deficit;
+
+    public OverDraftAmountException(double deficit, String message) {
+        super(message);
+        this.deficit = deficit;
+    }
+
+    public double getDeficit() {
+        return deficit;
+    }
+
+}
